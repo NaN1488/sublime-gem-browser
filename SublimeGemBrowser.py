64c6cd4968f0d913c87a4e12dff1a21598a6d1ed
Nan <nahuels@gmx.net>
2012-12-13 18:52:57 -0300

    list gems using bundle list for the current folder

--- SublimeGemBrowser.py
+++ SublimeGemBrowser.py
@@ -5,21 +5,29 @@ import subprocess
 import functools
 import os.path
 import time
-
+import re
 
 class ListGemsCommand(sublime_plugin.WindowCommand):
     """
     A command that shows a list of all installed gems (by bundle list command)
     """
-
+    
     def run(self):
         os.chdir(os.path.dirname(self.window.active_view().file_name()))
-        bashCommand = "bundle list"
+        bashCommand = "bundle list "
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
         output = process.communicate()[0]
+        output = output.split('\n')
+        gems = []
+        for line in output:
+            gem_name = re.search("\* (.+) \(", line)
+            if gem_name != None:
+                gems.append(gem_name.group(1))
+
+        if gems == []:
+            gems.append('Gems Not Found')
         
-        self.gem_list = output.split(' ')
-        
+        self.gem_list = gems
         self.window.show_quick_panel(self.gem_list, self.on_done)
 
     def on_done(self, picked):
