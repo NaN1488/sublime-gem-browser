e85168d53abc2b96668f272d979e8f5f98770637
Bruno Bonamin <bruno@bonamin.org>
2012-12-14 11:08:21 -0300

    Uses rvm-shell inside the current user to run bundle

--- SublimeGemBrowser.py
+++ SublimeGemBrowser.py
@@ -13,12 +13,8 @@ class ListGemsCommand(sublime_plugin.WindowCommand):
     A command that shows a list of all installed gems (by bundle list command)
     """
     
-    def run(self):
-        os.chdir(os.path.dirname(self.window.active_view().file_name()))
-        bashCommand = "bundle list "
-        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, shell=True)
-        output = process.communicate()[0]
-        output = output.split('\n')
+    def run(self):        
+        output = self.rvm_subprocess("bundle list").split('\n')
         gems = []
         for line in output:
             gem_name = re.search("\* (.+) \(", line)
@@ -32,10 +28,9 @@ class ListGemsCommand(sublime_plugin.WindowCommand):
         self.window.show_quick_panel(self.gem_list, self.on_done)
 
     def on_done(self, picked):
-      bashCommand = "bundle show " + self.gem_list[picked]
-      process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE,shell=True)
-      output = process.communicate()[0]
-      self.sublime_command_line(['-n', output.rstrip()]) 
+      if picked != -1 and self.gem_list[picked] != 'Gems Not Found': # Continue only if an actual gem was selected
+        output = self.rvm_subprocess("bundle show " + self.gem_list[picked])
+        self.sublime_command_line(['-n', output.rstrip()]) 
 
     def get_sublime_path(self):
         if sublime.platform() == 'osx':
@@ -43,6 +38,15 @@ class ListGemsCommand(sublime_plugin.WindowCommand):
         if sublime.platform() == 'linux':
             return open('/proc/self/cmdline').read().split(chr(0))[0]
         return sys.executable
+
+    def rvm_subprocess(self, args):
+        user = subprocess.Popen('whoami', stdout=subprocess.PIPE, shell=True)
+        user = user.communicate()[0].rsplit()[0]
+        current_path = os.path.dirname(self.window.active_view().file_name())
+        executable = '/Users/'+ user + '/.rvm/bin/rvm-shell'
+        args = 'cd ' + current_path + ';' + args
+        process = subprocess.Popen(args, stdout=subprocess.PIPE, shell=True, executable= executable)
+        return process.communicate()[0]
 
     def sublime_command_line(self, args):
         args.insert(0, self.get_sublime_path())
