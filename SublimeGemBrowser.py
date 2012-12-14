bbc1d886b5022264591bad68bce537ba130d1e9c
Bruno Bonamin <bruno@bonamin.org>
2012-12-14 16:10:02 -0300

    Better error handling when rvm is not found

--- SublimeGemBrowser.py
+++ SublimeGemBrowser.py
@@ -15,19 +15,21 @@ class ListGemsCommand(sublime_plugin.WindowCommand):
     GEMS_NOT_FOUND = 'Gems Not Found'
     
     def run(self):        
-        output = self.rvm_subprocess("bundle list").split('\n')
-        gems = []
-        for line in output:
-            gem_name_version = re.search(self.PATTERN_GEM_VERSION, line)
-            if gem_name_version != None:
-                gems.append(gem_name_version.group(1))
+        output = self.rvm_subprocess("bundle list")
+        if output != None:
+          gems = []
+          for line in output.split('\n'):
+              gem_name_version = re.search(self.PATTERN_GEM_VERSION, line)
+              if gem_name_version != None:
+                  gems.append(gem_name_version.group(1))
 
-        if gems == []:
-            gems.append(self.GEMS_NOT_FOUND)
+          if gems == []:
+              gems.append(self.GEMS_NOT_FOUND)
 
-        self.gem_list = gems
-        self.window.show_quick_panel(self.gem_list, self.on_done)
-
+          self.gem_list = gems
+          self.window.show_quick_panel(self.gem_list, self.on_done)
+        else:
+          print 'Error getting the output, the shell could probably not be loaded.'
     def on_done(self, picked):
         if self.gem_list[picked] != self.GEMS_NOT_FOUND and picked != -1:
             gem_name = re.search(self.PATTERN_GEM_NAME,self.gem_list[picked]).group(1)
@@ -44,7 +46,7 @@ class ListGemsCommand(sublime_plugin.WindowCommand):
 
     def rvm_subprocess(self, args):
        
-        executable = self.rvm_shell_path()
+        executable = self.ruby_environment()
         if executable != False:
             current_path = self.window.folders()[0]
             args = 'cd ' + current_path + ';' + args
@@ -52,11 +54,11 @@ class ListGemsCommand(sublime_plugin.WindowCommand):
             return process.communicate()[0]
 
     #return rvm shell path or False
-    def rvm_shell_path(self):
+    def ruby_environment(self):
         rvm_shell = subprocess.Popen(" if [ -f $HOME/.rvm/bin/rvm-shell ]; then echo $HOME/.rvm/bin/rvm-shell; fi", stdout=subprocess.PIPE, shell=True)
-        rvm_shell_path = rvm_shell.communicate()[0].rsplit()[0]
+        rvm_shell_path = rvm_shell.communicate()[0]
         if rvm_shell_path != '':
-            return rvm_shell_path.split('\n')[0]
+            return rvm_shell_path.rsplit()[0].split('\n')[0]
         return False
 
     def sublime_command_line(self, args):
