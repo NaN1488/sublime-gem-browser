4b556b7549f75dcc9f6451c0e230f409012de1f9
Nan <nahuels@gmx.net>
2012-12-14 12:57:09 -0300

    add gem version on the list

--- SublimeGemBrowser.py
+++ SublimeGemBrowser.py
@@ -12,26 +12,28 @@ class ListGemsCommand(sublime_plugin.WindowCommand):
     """
     A command that shows a list of all installed gems (by bundle list command)
     """
-    PATTERN_GEM = "\* (.+) \("
+    PATTERN_GEM_VERSION = "\* (.*)"
+    PATTERN_GEM_NAME = "(.*)\("
     GEMS_NOT_FOUND = 'Gems Not Found'
     
     def run(self):        
         output = self.rvm_subprocess("bundle list").split('\n')
         gems = []
         for line in output:
-            gem_name = re.search(self.PATTERN_GEM, line)
-            if gem_name != None:
-                gems.append(gem_name.group(1))
+            gem_name_version = re.search(self.PATTERN_GEM_VERSION, line)
+            if gem_name_version != None:
+                gems.append(gem_name_version.group(1))
 
         if gems == []:
             gems.append(self.GEMS_NOT_FOUND)
-        
+
         self.gem_list = gems
         self.window.show_quick_panel(self.gem_list, self.on_done)
 
     def on_done(self, picked):
       if self.gem_list[picked] != self.GEMS_NOT_FOUND:
-          bashCommand = "bundle show " + self.gem_list[picked]
+          gem_name = re.search(self.PATTERN_GEM_NAME,self.gem_list[picked]).group(1)
+          bashCommand = "bundle show " + gem_name
           output = self.rvm_subprocess(bashCommand)
           self.sublime_command_line(['-n', output.rstrip()]) 
 
